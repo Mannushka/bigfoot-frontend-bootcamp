fb9fbe556b1ab4b492e06d204dba9c633e572163
Mannushka <zaimar1910@gmail.com>
2024-02-25 21:19:21 +0800

    added a new route /new

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SightingPage from "./Components/SightingPage";
 import SightingsList from "./Components/SightingsList";
+import NewSightingForm from "./Components/NewSightingForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +16,7 @@ root.render(
         <Route index element={<SightingsList />}></Route>
         <Route path=":id" element={<SightingPage />}></Route>
       </Route>
+      <Route path="/new" element={<NewSightingForm />}></Route>
       <Route path="*" element={"Sorry, page not found!"} />
     </Routes>
   </Router>
