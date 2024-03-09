5a71402b4c766891bb29651409bbb3f7a5a17e0a
Mannushka <zaimar1910@gmail.com>
2024-03-09 19:16:14 +0800

    move sighting card into a separate component

---
+++ src/Components/SightingCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardContent } from "@mui/material";
+
+export default function SightingCard(props) {
+  console.log(props.sighting.date);
+  return (
+    <div className="sighting-card">
+      <Card
+        sx={{
+          width: 800,
+          minHeight: 300,
+        }}
+      >
+        <CardContent>
+          {props.categories.length > 0 && (
+            <p>Categories: {props.categories.join(", ")}</p>
+          )}
+          <p>Date: {props.sighting.date}</p>
+          <p>Location: {props.sighting.location}</p>
+          <p>{props.sighting.notes}</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
